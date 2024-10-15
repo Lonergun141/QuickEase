07bc1318cbb2b1f03d3643aa408a19dc940ccd16
Clyde H. Gevero <gevero530@gmail.com>
2024-10-15 18:15:01 +0800

    activation

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: "/QuickEase",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    port: 6969
-  }
-})
+	plugins: [react()],
+	base: "/QuickEase/"
+});
